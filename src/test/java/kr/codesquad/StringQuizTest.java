60b4a915d9cc266603e5b938c2073ef7e9789038
성필 <philip000412@gmail.com>
2025-01-08 12:11:24 +0900

    test: 테스트 코드 작성

--- src/test/java/kr/codesquad/StringQuizTest.java
+++ src/test/java/kr/codesquad/StringQuizTest.java
@@ -3,6 +3,9 @@ package kr.codesquad;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringQuizTest {
@@ -13,6 +16,7 @@ class StringQuizTest {
         String abc1 = "abc";
         String abc2 = new String("abc");
         // TODO 1 변수에 정의한 문자열과 new 를 통해 생성한 인스턴스가 같은 주소를 가리키는지 비교
+        assertNotSame(abc1, abc2);
     }
 
     @Test
@@ -20,6 +24,7 @@ class StringQuizTest {
     public void test_stringLongLong() throws Exception {
         String teacher = "Honux";
         // TODO 2 문자열 길이 구하기
+        assertEquals(teacher.length(), 5);
     }
 
     @Test
@@ -28,6 +33,8 @@ class StringQuizTest {
         String teacher = "Crong";
         String frontend = "Hello";
         // TODO 3 문자열 합치기
+        String newString = teacher + frontend;
+        assertEquals(newString, "CrongHello");
     }
 
     @Test
@@ -40,6 +47,9 @@ class StringQuizTest {
         // o
         // b
         // i
+        for (char ch : teacher.toCharArray()) {
+            System.out.println(ch);
+        }
     }
 
     @Test
@@ -49,6 +59,17 @@ class StringQuizTest {
 
         // TODO 5 더하기 연산을 이용해 String을 거꾸로 출력하기
         // 결과: elgnaD
+        String newString1 = "";
+        for (char ch : teacher.toCharArray()) {
+            newString1 = ch + newString1;
+        }
+        System.out.println(newString1);
+
+        String newString2 = "";
+        for (int i = teacher.length() - 1; i >= 0; i--) {
+            newString2 += teacher.charAt(i);
+        }
+        System.out.println(newString2);
     }
 
 
@@ -58,7 +79,22 @@ class StringQuizTest {
         String developer = "Zello";
         StringBuilder sb = new StringBuilder();
 
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
         // TODO 6 StringBuilder를 이용해 String을 거꾸로 출력하기
         // 결과: olleZ
+        for (int i = developer.length() - 1; i >= 0; i--) {
+            sb.append(developer.charAt(i));
+        }
+        System.out.println(sb);
+
+        assertEquals("olleZ", outputStream.toString().trim());
+
+        sb.delete(0, sb.length());
+        for (char ch : developer.toCharArray()) {
+            sb.insert(0, ch);
+        }
+        System.out.println(sb);
     }
 }
